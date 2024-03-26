4c69b9d847d0c36fb3220ec47c74f7d7b3adda53
Jahongir <boltayeb.jahongir@gmail.com>
2024-03-26 16:48:06 +0500

    asd

--- config/settings.py
+++ config/settings.py
@@ -123,8 +123,8 @@ USE_I18N = True
 
 USE_TZ = True
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = "media-files"
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media-files')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
